648e3916210f082497f9e5224426bc7c70be72cc
g-aleprojetos <g.aleprojetos@gmail.com>
2021-06-28 16:55:28 -0300

    Parte 1/3 concluida

---
+++ encriptar-senha-usuario/src/main/java/expertostech/encriptarsenhausuario/EncriptarSenhaUsuarioApplication.java
@@ -0,0 +1,13 @@
+package expertostech.encriptarsenhausuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncriptarSenhaUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncriptarSenhaUsuarioApplication.class, args);
+	}
+
+}
